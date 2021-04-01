ffa5506ea59db3aa2a393b6f5c8baa339d65073b
Robert Serrano Kobylyansky <sk.robchik@gmail.com>
2021-04-01 15:42:24 -0700

    structure

--- src/messages.rs
+++ src/messages.rs
@@ -7,3 +7,15 @@ pub struct PushDBMsg {
     pub value: f32,
     pub timestamp: u64
 }
+
+#[derive(Message, Debug, Clone)]
+#[rtype("()")]
+pub struct UpdateTelemetryMessage {
+    pub json_data: serde_json::Value,
+}
+
+impl UpdateTelemetryMessage {
+    pub fn from(data: serde_json::Value) -> UpdateTelemetryMessage {
+        UpdateTelemetryMessage { json_data: data }
+    }
+}
