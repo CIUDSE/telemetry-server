69915859d96fb72ae468b4914e55ab6120848f3a
Robert Serrano Kobylyansky <sk.robchik@gmail.com>
2021-04-01 22:45:48 -0700

    restructure

--- src/actors/mod.rs
+++ src/actors/mod.rs
@@ -1,5 +1,6 @@
 mod database;
 mod telemetry;
+mod common;
 
 pub use database::DBActor;
 pub use telemetry::InjestSocket;
--- src/actors/telemetry.rs
+++ src/actors/telemetry.rs
@@ -1,67 +1,16 @@
 use std::{
     error::Error,
-    time::{Duration, Instant, UNIX_EPOCH},
+    time::{Instant, UNIX_EPOCH},
     collections::HashSet
 };
 use actix::prelude::*;
 use actix_web::web;
-use actix_web_actors::ws::{self, WebsocketContext};
+use actix_web_actors::ws;
 use log::{debug, warn};
 use serde_json::json;
 use crate::messages::*;
 use crate::data::*;
-
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-
-trait Heartbeat {
-    fn get_heartbeat(&self) -> Instant;
-    fn refresh_heartbeat(&mut self);
-}
-
-fn heartbeat<A: Actor<Context = WebsocketContext<A>> + Heartbeat>(ctx: &mut WebsocketContext<A>) {
-    ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-        if act.get_heartbeat().elapsed() > CLIENT_TIMEOUT {
-            // heartbeat timed out
-            warn!("Websocket Client heartbeat failed, disconnecting!");
-            // stop actor
-            ctx.stop();
-            // don't try to send a ping
-            return;
-        }
-        ctx.ping(b"");
-    });
-}
-
-fn ws_handler<A,F>(
-    act: &mut A,
-    msg: Result<ws::Message, ws::ProtocolError>,
-    ctx: &mut WebsocketContext<A>,
-    text_handler: F)
-where
-    A: Actor<Context = WebsocketContext<A>> + Heartbeat,
-    F: Fn(String, &mut WebsocketContext<A>, &mut A)
-{
-    match msg {
-        Ok(ws::Message::Ping(msg)) => {
-            act.refresh_heartbeat();
-            ctx.pong(&msg);
-        }
-        Ok(ws::Message::Pong(_)) => {
-            act.refresh_heartbeat();
-        }
-        Ok(ws::Message::Text(text)) => {
-            text_handler(text, ctx, act);
-        }
-        Ok(ws::Message::Binary(_bin)) => {}
-        Ok(ws::Message::Close(reason)) => {
-            ctx.close(reason);
-            ctx.stop();
-        }
-        _ => ctx.stop(),
-    }
-}
-
+use crate::actors::common::*;
 
 #[derive(Debug)]
 pub struct InjestSocket {
